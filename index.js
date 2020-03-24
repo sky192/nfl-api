b93edef59ddfcdb24b719cf3ca3cfb9cd5dda947
John Carmichael <jc@stackeducation.com>
2020-03-24 09:22:34 -0400

    The solution to Part Two of this exercise

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const { getAllTeams, getTeamById } = require('./controllers/teams')
+const bodyParser = require('body-parser')
+const { getAllTeams, getTeamById, saveNewTeam } = require('./controllers/teams')
 
 const app = express()
 
 app.get('/', getAllTeams)
 
 app.get('/:id', getTeamById)
+
+app.post('/', bodyParser.json(), saveNewTeam)
 
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
