7f6db7fb420c7dabd0be592e9f2eb96babdb16db
John Carmichael <jc@stackeducation.com>
2020-04-11 11:33:46 -0400

    The solution to Part Five of this exercise

---
+++ tests/mocks/teams.js
@@ -0,0 +1,34 @@
+const singleTeam = {
+  id: 1,
+  location: 'Buffalo',
+  mascot: 'Bills',
+  abbreviation: 'BUF',
+  conference: 'AFC',
+  division: 'East'
+}
+
+const postedTeam = {
+  location: 'Buffalo',
+  mascot: 'Bills',
+  abbreviation: 'BUF',
+  conference: 'AFC',
+  division: 'East'
+}
+
+const teamsList = [{
+  id: 2,
+  location: 'Miami',
+  mascot: 'Dolphins',
+  abbreviation: 'MIA',
+  conference: 'AFC',
+  division: 'East'
+}, {
+  id: 3,
+  location: 'New England',
+  mascot: 'Patriots',
+  abbreviation: 'NE',
+  conference: 'AFC',
+  division: 'East'
+}]
+
+module.exports = { postedTeam, singleTeam, teamsList }
