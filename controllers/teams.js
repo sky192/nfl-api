b0697833ca01d063a775c96eb175fe508fa63887
John Carmichael <jc@stackeducation.com>
2020-04-11 11:36:50 -0400

    The solution to Part Six of this exercise

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,37 +1,49 @@
 const models = require('../models')
 
 const getAllTeams = async (request, response) => {
-  const teams = await models.Teams.findAll()
+  try {
+    const teams = await models.Teams.findAll()
 
-  return response.send(teams)
+    return response.send(teams)
+  } catch (error) {
+    return response.status(500).send('Unable to retrieve team list, please try again')
+  }
 }
 
 const getTeamById = async (request, response) => {
-  const { id } = request.params
+  try {
+    const { id } = request.params
 
-  const matchingTeam = await models.Teams.findOne({ where: { id } })
+    const matchingTeam = await models.Teams.findOne({ where: { id } })
 
-  return matchingTeam
-    ? response.send(matchingTeam)
-    : response.sendStatus(404)
+    return matchingTeam
+      ? response.send(matchingTeam)
+      : response.sendStatus(404)
+  } catch (error) {
+    return response.status(500).send('Unable to retrieve team, please try again')
+  }
 }
 
 const saveNewTeam = async (request, response) => {
-  const {
-    location, mascot, abbreviation, conference, division
-  } = request.body
+  try {
+    const {
+      location, mascot, abbreviation, conference, division
+    } = request.body
 
-  if (!location || !mascot || !abbreviation || !conference || !division) {
-    return response
-      .status(400)
-      .send('The following fields are required: location, mascot, abbreviation, conference, division')
+    if (!location || !mascot || !abbreviation || !conference || !division) {
+      return response
+        .status(400)
+        .send('The following fields are required: location, mascot, abbreviation, conference, division')
+    }
+
+    const newTeam = await models.Teams.create({
+      location, mascot, abbreviation, conference, division
+    })
+
+    return response.status(201).send(newTeam)
+  } catch (error) {
+    return response.status(500).send('Unable to save team, please try again')
   }
-
-  const newTeam = await models.Teams.create({
-    location, mascot, abbreviation, conference, division
-  })
-
-  return response.status(201).send(newTeam)
 }
 
 module.exports = { getAllTeams, getTeamById, saveNewTeam }
--- tests/controllers/teams.test.js
+++ tests/controllers/teams.test.js
@@ -3,7 +3,9 @@ const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const models = require('../../models')
-const { describe, it } = require('mocha')
+const {
+  afterEach, before, beforeEach, describe, it
+} = require('mocha')
 const { postedTeam, singleTeam, teamsList } = require('../mocks/teams')
 const { getAllTeams, getTeamById, saveNewTeam } = require('../../controllers/teams')
 
@@ -11,46 +13,134 @@ chai.use(sinonChai)
 const { expect } = chai
 
 describe('Controllers - Teams', () => {
+  let response
+  let sandbox
+  let stubbedCreate
+  let stubbedFindOne
+  let stubbedFindAll
+  let stubbedSend
+  let stubbedSendStatus
+  let stubbedStatus
+  let stubbedStatusDotSend
+
+  before(() => {
+    sandbox = sinon.createSandbox()
+
+    stubbedFindAll = sandbox.stub(models.Teams, 'findAll')
+    stubbedFindOne = sandbox.stub(models.Teams, 'findOne')
+    stubbedCreate = sandbox.stub(models.Teams, 'create')
+
+    stubbedSend = sandbox.stub()
+    stubbedSendStatus = sandbox.stub()
+    stubbedStatusDotSend = sandbox.stub()
+    stubbedStatus = sandbox.stub()
+
+    response = {
+      send: stubbedSend,
+      sendStatus: stubbedSendStatus,
+      status: stubbedStatus,
+    }
+  })
+
+  beforeEach(() => {
+    stubbedStatus.returns({ send: stubbedStatusDotSend })
+  })
+
+  afterEach(() => {
+    sandbox.reset()
+  })
+
+
   describe('getAllTeams', () => {
     it('retrieves a list of teams from the database and calls response.send() with the list', async () => {
-      const stubbedFindAll = sinon.stub(models.Teams, 'findAll').returns(teamsList)
-      const stubbedSend = sinon.stub()
-      const response = { send: stubbedSend }
+      stubbedFindAll.returns(teamsList)
 
       await getAllTeams({}, response)
 
       expect(stubbedFindAll).to.have.callCount(1)
       expect(stubbedSend).to.have.been.calledWith(teamsList)
     })
+
+    it('returns a 500 status when an error occurs retrieving the teams', async () => {
+      stubbedFindAll.throws('ERROR!')
+
+      await getAllTeams({}, response)
+
+      expect(stubbedFindAll).to.have.callCount(1)
+      expect(stubbedStatus).to.have.been.calledWith(500)
+      expect(stubbedStatusDotSend).to.have.been.calledWith('Unable to retrieve team list, please try again')
+    })
   })
 
   describe('getTeamById', () => {
     it('retrieves the team associated with the provided slug from the database and calls response.send() with it', async () => {
       const request = { params: { id: '1' } }
-      const stubbedSend = sinon.stub()
-      const response = { send: stubbedSend }
-      const stubbedFindOne = sinon.stub(models.Teams, 'findOne').returns(singleTeam)
+
+      stubbedFindOne.returns(singleTeam)
 
       await getTeamById(request, response)
 
       expect(stubbedFindOne).to.have.been.calledWith({ where: { id: '1' } })
       expect(stubbedSend).to.have.been.calledWith(singleTeam)
     })
+
+    it('returns a 404 status when no team is found', async () => {
+      const request = { params: { id: '1' } }
+
+      stubbedFindOne.returns(null)
+
+      await getTeamById(request, response)
+
+      expect(stubbedSendStatus).to.have.been.calledWith(404)
+    })
+
+    it('returns a 500 status when an error occurs retrieving the team by id', async () => {
+      const request = { params: { id: '1' } }
+
+      stubbedFindOne.throws('ERROR!')
+
+      await getTeamById(request, response)
+
+      expect(stubbedFindOne).to.have.been.calledWith({ where: { id: '1' } })
+      expect(stubbedStatus).to.have.been.calledWith(500)
+      expect(stubbedStatusDotSend).to.have.been.calledWith('Unable to retrieve team, please try again')
+    })
   })
 
   describe('saveNewTeam', () => {
     it('accepts new team details and saves them as a new team in the database, returning the saved record with a 201 status', async () => {
       const request = { body: postedTeam }
-      const stubbedSend = sinon.stub()
-      const stubbedStatus = sinon.stub().returns({ send: stubbedSend })
-      const response = { status: stubbedStatus }
-      const stubbedCreate = sinon.stub(models.Teams, 'create').returns(singleTeam)
+
+      stubbedCreate.returns(singleTeam)
 
       await saveNewTeam(request, response)
 
       expect(stubbedCreate).to.have.been.calledWith(postedTeam)
       expect(stubbedStatus).to.have.been.calledWith(201)
-      expect(stubbedSend).to.have.been.calledWith(singleTeam)
+      expect(stubbedStatusDotSend).to.have.been.calledWith(singleTeam)
+    })
+
+    it('returns a 400 status when not all required fields are provided (missing location)', async () => {
+      const { mascot, abbreviation, conference, division } = postedTeam
+      const request = { body: { mascot, abbreviation, conference, division } }
+
+      await saveNewTeam(request, response)
+
+      expect(stubbedCreate).to.have.callCount(0)
+      expect(stubbedStatus).to.have.been.calledWith(400)
+      expect(stubbedStatusDotSend).to.have.been.calledWith('The following fields are required: location, mascot, abbreviation, conference, division')
+    })
+
+    it('returns a 500 status when an error occurs saving the new team', async () => {
+      const request = { body: postedTeam }
+
+      stubbedCreate.throws('ERROR!')
+
+      await saveNewTeam(request, response)
+
+      expect(stubbedCreate).to.have.been.calledWith(postedTeam)
+      expect(stubbedStatus).to.have.been.calledWith(500)
+      expect(stubbedStatusDotSend).to.have.been.calledWith('Unable to save team, please try again')
     })
   })
 })
